65e464610aae941b63a8ab936d343c2ef21362ab
Sreenath <sreenathkumar66@gmail.com>
2023-12-17 22:22:34 +0600

    Feat: added the post, patch and delete routes

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,8 +1,10 @@
-import { getProfilesController } from '../controllers/profileControllers';
+import { createProfileController, deleteProfileController, getProfilesController, updateProfileController } from '../controllers/profileControllers';
 import express, { Router } from 'express'
 const router: Router = express.Router();
 
-router.get('/profile', getProfilesController);
-router.post('/profile',);
+router.get('/profile', getProfilesController); // get the profile of a user
+router.post('/profile', createProfileController); // create the profile of a user
+router.patch('/profile', updateProfileController); // update the profile of a user
+router.delete('/profile', deleteProfileController); // delete the profile of a user
 
 export default router;

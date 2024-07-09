4021c3939be92ff1024fc8f03356df5346762c3c
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-07-09 22:55:45 +0600

    test: added middleware in get image router

--- src/routes/imageRoutes.ts
+++ src/routes/imageRoutes.ts
@@ -6,6 +6,12 @@ import { upload } from '../middlewires/uploadImageMiddlewire';
 const router: Router = express.Router();
 
 router.post('/upload', upload.any(), uploadImageController);
-router.get('/photos', getImageController);
+//router.get('/photos', getImageController);
+
+router.get('/photos', (req, res, next) => {
+    console.log("GET /photos endpoint hit");
+    next();
+}, getImageController);
+
 
 export default router;

c0c303ffdd769ecd17c0b379cfd4681692999b44
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-07-10 19:59:24 +0600

    Refactor: remove console logs and refactorize the getImages function

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -1,71 +1,111 @@
-import express from 'express';
-import { Stream } from 'stream';
-import fs from 'fs';
-import { blobServiceClient, downloadBlob } from '../utils/azure';
-import { getFileExtension } from '../utils/others';
+import express from "express";
+import { Stream } from "stream";
+import fs from "fs";
+import { blobServiceClient, downloadBlob } from "../utils/azure";
+import { getFileExtension } from "../utils/others";
 
-
-if (!process.env.AZURE_STORAGE_PHOTO_CONTAINER_NAME || !process.env.AZURE_STORAGE_SIGNATURE_CONTAINER_NAME) {
-   throw Error('Azure Storage container name defined');
+if (
+  !process.env.AZURE_STORAGE_PHOTO_CONTAINER_NAME ||
+  !process.env.AZURE_STORAGE_SIGNATURE_CONTAINER_NAME
+) {
+  throw Error("Azure Storage container name defined");
 }
 // ContainerClient in which the image will be stored
-const photoContainerClient = blobServiceClient.getContainerClient(process.env.AZURE_STORAGE_PHOTO_CONTAINER_NAME);
-const signatureContainerClient = blobServiceClient.getContainerClient(process.env.AZURE_STORAGE_SIGNATURE_CONTAINER_NAME);
+const photoContainerClient = blobServiceClient.getContainerClient(
+  process.env.AZURE_STORAGE_PHOTO_CONTAINER_NAME
+);
+const signatureContainerClient = blobServiceClient.getContainerClient(
+  process.env.AZURE_STORAGE_SIGNATURE_CONTAINER_NAME
+);
 
-export const uploadImageController = async (req: express.Request, res: express.Response) => {
-   try {
-      const files = req.files as Express.Multer.File[];
-      for (const file of files) {
+export const uploadImageController = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const files = req.files as Express.Multer.File[];
+    for (const file of files) {
+      // Create readable stream from the file buffer
+      const readableStream = new Stream.PassThrough();
+      readableStream.end(file.buffer);
 
-         // Create readable stream from the file buffer
-         const readableStream = new Stream.PassThrough();
-         readableStream.end(file.buffer);
+      //Upload the image to the container
+      const blobName = `${req.body.id}_${file.fieldname}.${getFileExtension(
+        file.originalname
+      )}`;
+      let uploadResponse; // will be checked if the image is uploaded successfully or not
 
-         //Upload the image to the container
-         const blobName = `${req.body.id}_${file.fieldname}.${getFileExtension(file.originalname)}`;
-         let uploadResponse; // will be checked if the image is uploaded successfully or not
-
-         if (file.fieldname === 'photo') {
-            const blobClient = photoContainerClient.getBlockBlobClient(blobName);
-            uploadResponse = await blobClient.uploadFile(file.path);
-         } else if (file.fieldname === 'signature') {
-            const blobClient = signatureContainerClient.getBlockBlobClient(blobName);
-            uploadResponse = await blobClient.uploadFile(file.path);
-         }
-
-         console.log(`${file.originalname} uploaded successfully to the blob storage`);
-
-         if (uploadResponse) {
-            //Delete the image from the uploads folder
-            fs.unlink(file.path, (unlinkError) => {
-               if (unlinkError) {
-                  console.log(`Error deleting ${file.fieldname}: ${unlinkError}`);
-               } else {
-                  console.log(`${file.fieldname} deleted successfully`);
-               }
-            });
-         }
+      if (file.fieldname === "photo") {
+        const blobClient = photoContainerClient.getBlockBlobClient(blobName);
+        uploadResponse = await blobClient.uploadFile(file.path);
+      } else if (file.fieldname === "signature") {
+        const blobClient =
+          signatureContainerClient.getBlockBlobClient(blobName);
+        uploadResponse = await blobClient.uploadFile(file.path);
       }
-      res.send({ status: 'success', message: `Images uploaded successfully to the blob storage` });
-   } catch (err) {
-      res.send({ status: 'error', error: `Error uploading image: ${err}` });
-   }
-}
 
-export const getImageController = async (req: express.Request, res: express.Response) => {
-   const { id } = req.query as { id: string };
-   if (!id) {
-      throw Error('id not defined');
-   }
+      if (uploadResponse) {
+        //Delete the image from the uploads folder
+        fs.unlink(file.path, (unlinkError) => {
+          if (unlinkError) {
+            console.log(`Error deleting ${file.fieldname}: ${unlinkError}`);
+          } else {
+            console.log(`${file.fieldname} deleted successfully`);
+          }
+        });
+      }
+    }
+    res.send({
+      status: "success",
+      message: `Images uploaded successfully to the blob storage`,
+    });
+  } catch (err) {
+    res.send({ status: "error", error: `Error uploading image: ${err}` });
+  }
+};
 
-   try {
-      const photoResponse = await downloadBlob(photoContainerClient, `${id}_photo.jpg`);// get the image from the container
-      const signatureResponse = await downloadBlob(signatureContainerClient, `${id}_signature.jpg`); // get the signature from the container
+export const getImageController = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  const { id } = req.query as { id: string };
+  if (!id) {
+    throw Error("id not defined");
+  }
 
-      console.log(photoResponse, signatureResponse);
-      res.send({ status: 'success', photo: photoResponse.toString('base64'), signature: signatureResponse.toString('base64') });
-   } catch (err) {
-      console.log(err);
-      res.send({ status: 'error', error: `Error getting image: ${err}` });
-   }
-}
+  try {
+    let photoResponse = null;
+    let signatureResponse = null;
+
+    try {
+      photoResponse = await downloadBlob(
+        photoContainerClient,
+        `${id}_photo.jpg`
+      );
+    } catch (photoError: any) {
+      console.log("Error downloading photo:", photoError?.details?.errorCode);
+    }
+
+    try {
+      signatureResponse = await downloadBlob(
+        signatureContainerClient,
+        `${id}_signature.jpg`
+      );
+    } catch (signatureError: any) {
+      console.log(
+        "Error downloading signature:",
+        signatureError?.details?.errorCode
+      );
+    }
+
+    res.send({
+      status: "success",
+      photo: photoResponse ? photoResponse.toString("base64") : null,
+      signature: signatureResponse
+        ? signatureResponse.toString("base64")
+        : null,
+    });
+  } catch (err) {
+    res.send({ status: "error", error: `Error getting image: ${err}` });
+  }
+};
--- src/controllers/profileControllers.ts
+++ src/controllers/profileControllers.ts
@@ -1,28 +1,26 @@
-import express from 'express';
-import { User } from '../models/userModels';
-import { UserType } from '../utils/types';
+import express from "express";
+import { User } from "../models/userModels";
+import { UserType } from "../utils/types";
 
-export const getProfilesController = async (req: express.Request, res: express.Response) => {
-   try {
-      const { user_id } = req.query as { user_id: string };
-      console.log(user_id);
+export const getProfilesController = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { user_id } = req.query as { user_id: string };
 
-      // if (!user_id) {
-      //    res.send({ status: 'error', message: 'user_id not found' });
-      //    return;
-      // };
-      const user = await User.find({});
-      console.log(user);
-      if (Object.keys(user).length === 0) {
-         throw Error('user not found');
-      };
-      console.log('hello')
-      //console.log(Object.keys(user).length);
+    // if (!user_id) {
+    //    res.send({ status: 'error', message: 'user_id not found' });
+    //    return;
+    // };
+    const user = await User.find({});
+    if (Object.keys(user).length === 0) {
+      throw Error("user not found");
+    }
+    //console.log(Object.keys(user).length);
+  } catch (error) {
+    //console.log(error);
 
-   } catch (error) {
-      //console.log(error);
-
-      res.send({ status: 'error', message: error });
-   }
-
+    res.send({ status: "error", message: error });
+  }
 };

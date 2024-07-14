751946171cd665a2a43e3ea264b928acc5983dc3
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-07-14 18:30:35 +0600

    Fix: added the missing routes that missed during merge

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,9 +1,11 @@
-import { getJobDataController } from "../controllers/getJobDataController";
-import { createProfileController, deleteProfileController, getProfilesController, updateProfileController } from "../controllers/profileControllers";
+import { createProfileController, deleteProfileController, getJobDataController, getProfilesController, updateProfileController } from "../controllers/profileControllers";
 import express, { Router } from "express";
 const router: Router = express.Router();
 
 router.get('/profile', getProfilesController);
-router.post('/profile',);
+router.post('/profile', createProfileController); // create the profile of a user
+router.patch('/profile', updateProfileController); // update the profile of a user
+router.delete('/profile', deleteProfileController); // delete the profile of a user
+router.post('/profile/job-data', getJobDataController);
 
 export default router;
